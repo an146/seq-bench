83db6d51354965ce9aa736ad1156faa196760f0b
Anton Novikov <an146@ya.ru>
2011-01-31 02:46:30 +0300

    written by hand implementation

--- seqbench.ml
+++ seqbench.ml
@@ -4,16 +4,23 @@ open Sys (* argv *)
 
 let _ =
    if Array.length argv < 3 then
-      failwith (sprintf "usage: %s count listify_first" argv.(0));
+      failwith (sprintf "usage: %s count <0|1|2>" argv.(0));
    let count = int_of_string argv.(1)
-   and listify_first = int_of_string argv.(2) in
-   let items = (1 -- count) in
+   and benchtype = int_of_string argv.(2) in
+   let resulting_list () =
+      let rec f i l =
+         if i <= 1 then l
+         else f (i - 1) (i :: l)
+      in
+      f (count + 1) []
+   in
    let f x = x + 1 in
    let incs = 
-      if listify_first > 0 then
-         items |> List.of_enum |> List.map f
-      else
-         items |> Enum.map f |> List.of_enum
+      match benchtype with
+      | 0 -> 1 -- count |> Enum.map f |> List.of_enum
+      | 1 -> 1 -- count |> List.of_enum |> List.map f
+      | 2 -> resulting_list ()
+      | _   -> failwith "no such bench type"
    in
    ignore incs
 
--- test.sh
+++ test.sh
@@ -8,3 +8,5 @@ echo processing enum, then converting to list
 do_test "time ./seqbench 3000000 0"
 echo converting to list, then use List.map on it
 do_test "time ./seqbench 3000000 1"
+echo written by hand
+do_test "time ./seqbench 3000000 2"
